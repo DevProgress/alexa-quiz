fcecc493124223ce2bfe074735523053c18c0f04
Kimberly Nicholls <kimberly@gridium.com>
2016-09-16 16:39:31 -0700

    save/load session to Firebase for between session persistence
    on launch, try to load session from Firebase
    on answer, save session to Firebase
    
    keep list of all answered questions in Firebase
    log answers to Firebase by question for stats

--- apps/candidate-quiz/index.js
+++ apps/candidate-quiz/index.js
@@ -1,20 +1,47 @@
+// jshint esversion: 6
+
 var alexa = require('alexa-app');
 var app = new alexa.app('candidatequiz');
 var quiz = require('./quiz');
+app.db = require('./db/mock-db');
 
+function getDatabase() {
+    return app.db;
+}
 
 app.launch(function(request, response) {
-    var say = ['<s>Welcome to candidate quiz twenty sixteen. <break strength="medium" /></s>'];
-    say.push('<s>First question:</s>');
-    var q = quiz.getNextQuestion();
-    if (q) {
-        say.push(q.questionAndAnswers());
-        response.session('q', q.id);
-        response.shouldEndSession(false, 'What do you think? Is it '+q.choices()+'?');
-    } else {
-        say.push("That's all the questions I have for now.  Remember to vote on November eighth.");
-    }
-    response.say(say.join('\n'));
+    app.db.loadSession(request.userId).then((savedSession) => {
+        var say = [];
+        var used = [];
+        // copy saved session into current session
+        var session = savedSession || {};
+        console.log('session=', session);
+        if (session) {
+            var all = JSON.parse(session.all || '{}');
+            used = Object.keys(all);
+            Object.keys(session).forEach((key) => {
+                response.session(key, savedSession[key]);
+            });
+            say.push('<s>Welcome back.  <break strength="medium" /></s>');
+            say.push('<s>Next question:</s>');
+        } else {
+            say.push('<s>Welcome to candidate quiz twenty sixteen. <break strength="medium" /></s>');
+            say.push('<s>First question:</s>');
+        }
+        // set current list of questions to empty
+        response.session('current', '{}');
+        var q = quiz.getNextQuestion(used);
+        if (q) {
+            say.push(q.questionAndAnswers());
+            response.session('q', q.id);
+            response.shouldEndSession(false, 'What do you think? Is it '+q.choices()+'?');
+        } else {
+            say.push("That's all the questions I have for now.  Remember to vote on November eighth.");
+        }
+        response.say(say.join('\n'));
+        response.send();
+    });
+    return false;  // wait for promise to resolve
 });
 
 app.intent('AMAZON.HelpIntent', function(request, response) {
@@ -23,11 +50,12 @@ app.intent('AMAZON.HelpIntent', function(request, response) {
 });
 
 app.intent('AMAZON.StopIntent', function(request, response) {
-    var correct = parseInt(request.session('correct') || '0');
+    var score = quiz.getScore(request.session('current') || {});
     var say = ['Thanks for playing candidate quiz. '];
-    if (correct) {
+    if (score) {
         say.push('You got '+correct+' correct. ');
     }
+    // TODO: display questions and answers on card
     say.push('Remember to vote on November eighth.');
     response.say(say.join('\n'));
 });
@@ -48,42 +76,51 @@ app.intent('AnswerIntent',
     },
 
     function(request, response) {
-        console.log('answer session=', request.sessionDetails);
-        var used = request.session('used') || '';
+        var session = request.sessionDetails.attributes;
+        console.log('answer session=', session);
+        // {'1': 'A', '2': 'false'}
+        var all = JSON.parse(request.session('all') || '{}');
+        var current = JSON.parse(request.session('current') || '{}');
+        var used = Object.keys(all);
         var currentQuestionId = request.session('q');
         var say = [];
         var q = currentQuestionId ? quiz.getQuestion(currentQuestionId) : null;
         console.log('answerIntent current=|'+currentQuestionId+'| used=|'+used+'|');
-        var correct = parseInt(request.session('correct') || '0');
+        var score = quiz.getScore(JSON.parse(request.session('current') || '{}'));
         // found question in session; check answer
         if (q) {
             var answer = request.slot('ANSWER');
+            console.log('answer='+answer);
+            app.db.logAnswer(currentQuestionId, answer);
             var sayAnswer = q.answer(answer);
             if (q.isCorrect(answer)) {
                 say.push('<s>'+sayAnswer+' is correct!</s>');
-                correct += 1;
+                score += 1;
             } else {
                 say.push('<s>The correct answer is '+q.answerText()+'.</s>');
             }
             say.push(q.explanation());
-            used += ','+currentQuestionId;
+            // save question and answer to current and all questions
+            current[currentQuestionId] = answer;
+            all[currentQuestionId] = answer;
         }
-        response.session('correct', correct+'');
-        // save list of used questions
-        used = used.trim();
-        console.log('answerIntent setting used to |'+used+'|');
-        response.session('used', used);
+        session.current = JSON.stringify(current);
+        session.all = JSON.stringify(all);
         // get next question
-        var next = quiz.getNextQuestion(used);
+        var next = quiz.getNextQuestion(Object.keys(all));
         if (next) {
             say.push('<s>Next question.</s>');
             say.push(next.questionAndAnswers());
-            response.session('q', next.id);
+            session.q = next.id;
             response.shouldEndSession(false, 'What do you think? Is it '+next.choices()+'?');
         } else {
-            say.push("That's all the questions I have for now. You got "+correct+
+            say.push("That's all the questions I have for now. You got "+score+
                 " correct. Remember to vote on November eighth.");
         }
+        Object.keys(session).forEach((key) => {
+            response.session(key, session[key]);
+        });
+        app.db.saveSession(request.userId, session);
         response.say(say.join('\n'));
     }
 );
